ed29091384874ade5e17602fc13096314b3e82e1
Rafsan Ahmad <rafsanahmad111@gmail.com>
2021-10-23 14:42:40 +0600

    state hoisting

--- app/src/main/java/com/rafsan/jetpackcomposebasics/MainActivity.kt
+++ app/src/main/java/com/rafsan/jetpackcomposebasics/MainActivity.kt
@@ -1,78 +1,114 @@
-package com.rafsan.jetpackcomposebasics
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.rafsan.jetpackcomposebasics.ui.theme.JetpackComposeBasicsTheme
+import com.rafsan.jetpackcomposebasics.ui.theme.Teal700
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeBasicsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    MyApp()
+                MyApp()
+            }
+        }
+    }
+
+    @Composable
+    fun MyApp() {
+
+        var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+        if (shouldShowOnboarding) {
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        } else {
+            Greetings()
+        }
+    }
+
+    @Composable
+    fun OnboardingScreen(onContinueClicked: () -> Unit) {
+
+        Surface {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("Welcome to the Jetpack Compose Basics!")
+                Button(
+                    modifier = Modifier.padding(vertical = 24.dp),
+                    onClick = onContinueClicked,
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = Teal700,
+                        contentColor = Color.White
+                    )
+                ) {
+                    Text("Continue")
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    val expanded = remember { mutableStateOf(false) }
-
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
-
-    Surface(
-        color = MaterialTheme.colors.primary,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
-    ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding)
-            ) {
-                Text(text = "Hello, ")
-                Text(text = name)
-            }
-            OutlinedButton(
-                onClick = { expanded.value = !expanded.value }
-            ) {
-                Text(if (expanded.value) "Show less" else "Show more")
+    @Composable
+    private fun Greetings(names: List<String> = listOf("World", "Compose")) {
+        Column(modifier = Modifier.padding(12.dp)) {
+            for (name in names) {
+                Greeting(name = name)
             }
         }
     }
-}
 
-@Composable
-fun MyApp(names: List<String> = listOf("World", "Compose")) {
-    Column(modifier = Modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
-            Greeting(name = name)
+    @Preview(showBackground = true, widthDp = 320, heightDp = 320)
+    @Composable
+    fun OnboardingPreview() {
+        JetpackComposeBasicsTheme {
+            OnboardingScreen(onContinueClicked = {})
         }
     }
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetpackComposeBasicsTheme {
-        MyApp()
+    @Composable
+    private fun Greeting(name: String) {
+
+        val expanded = remember { mutableStateOf(false) }
+
+        val extraPadding = if (expanded.value) 48.dp else 0.dp
+
+        Surface(
+            color = Teal700,
+            modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+        ) {
+            Row(modifier = Modifier.padding(24.dp)) {
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(bottom = extraPadding)
+                ) {
+                    Text(text = "Hello, ", color = Color.White)
+                    Text(text = name, color = Color.White)
+                }
+                OutlinedButton(
+                    onClick = { expanded.value = !expanded.value }
+                ) {
+                    Text(if (expanded.value) "Show less" else "Show more")
+                }
+            }
+        }
+    }
+
+    @Preview(showBackground = true, widthDp = 320)
+    @Composable
+    fun DefaultPreview() {
+        JetpackComposeBasicsTheme {
+            Greetings()
+        }
     }
 }
