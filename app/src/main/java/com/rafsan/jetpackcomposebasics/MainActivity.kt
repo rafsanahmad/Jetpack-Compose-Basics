8da047ee8891a404a5d7608210589c3971f4635e
Rafsan Ahmad <rafsanahmad111@gmail.com>
2021-10-23 16:24:47 +0600

    fix crash, padding

--- app/src/main/java/com/rafsan/jetpackcomposebasics/MainActivity.kt
+++ app/src/main/java/com/rafsan/jetpackcomposebasics/MainActivity.kt
@@ -1,3 +1,5 @@
+package com.rafsan.jetpackcomposebasics
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -21,94 +23,94 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
 
-    @Composable
-    fun MyApp() {
+@Composable
+fun MyApp() {
 
-        var shouldShowOnboarding by remember { mutableStateOf(true) }
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
 
-        if (shouldShowOnboarding) {
-            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
-        } else {
-            Greetings()
-        }
+    if (shouldShowOnboarding) {
+        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+    } else {
+        Greetings()
     }
+}
 
-    @Composable
-    fun OnboardingScreen(onContinueClicked: () -> Unit) {
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
 
-        Surface {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text("Welcome to the Jetpack Compose Basics!")
-                Button(
-                    modifier = Modifier.padding(vertical = 24.dp),
-                    onClick = onContinueClicked,
-                    colors = ButtonDefaults.buttonColors(
-                        backgroundColor = Teal700,
-                        contentColor = Color.White
-                    )
-                ) {
-                    Text("Continue")
-                }
-            }
-        }
-    }
-
-    @Composable
-    private fun Greetings(names: List<String> = listOf("World", "Compose")) {
-        Column(modifier = Modifier.padding(12.dp)) {
-            for (name in names) {
-                Greeting(name = name)
-            }
-        }
-    }
-
-    @Preview(showBackground = true, widthDp = 320, heightDp = 320)
-    @Composable
-    fun OnboardingPreview() {
-        JetpackComposeBasicsTheme {
-            OnboardingScreen(onContinueClicked = {})
-        }
-    }
-
-    @Composable
-    private fun Greeting(name: String) {
-
-        val expanded = remember { mutableStateOf(false) }
-
-        val extraPadding = if (expanded.value) 48.dp else 0.dp
-
-        Surface(
-            color = Teal700,
-            modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Row(modifier = Modifier.padding(24.dp)) {
-                Column(
-                    modifier = Modifier
-                        .weight(1f)
-                        .padding(bottom = extraPadding)
-                ) {
-                    Text(text = "Hello, ", color = Color.White)
-                    Text(text = name, color = Color.White)
-                }
-                OutlinedButton(
-                    onClick = { expanded.value = !expanded.value }
-                ) {
-                    Text(if (expanded.value) "Show less" else "Show more")
-                }
+            Text("Welcome to the Jetpack Compose Basics!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = onContinueClicked,
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Teal700,
+                    contentColor = Color.White
+                )
+            ) {
+                Text("Continue")
             }
         }
     }
+}
 
-    @Preview(showBackground = true, widthDp = 320)
-    @Composable
-    fun DefaultPreview() {
-        JetpackComposeBasicsTheme {
-            Greetings()
+@Composable
+private fun Greetings(names: List<String> = listOf("World", "Compose")) {
+    Column(modifier = Modifier.padding(12.dp)) {
+        for (name in names) {
+            Greeting(name = name)
         }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    JetpackComposeBasicsTheme {
+        OnboardingScreen(onContinueClicked = {})
+    }
+}
+
+@Composable
+private fun Greeting(name: String) {
+
+    val expanded = remember { mutableStateOf(false) }
+
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+
+    Surface(
+        color = Teal700,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
+                Text(text = "Hello, ", color = Color.White)
+                Text(text = name, color = Color.White)
+            }
+            OutlinedButton(
+                onClick = { expanded.value = !expanded.value }
+            ) {
+                Text(if (expanded.value) "Show less" else "Show more")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun DefaultPreview() {
+    JetpackComposeBasicsTheme {
+        Greetings()
     }
 }
